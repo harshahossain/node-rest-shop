5aa879300d116094c164627ba81983c1db44cbcb
harshahossain <harshahossain@gmail.com>
2023-11-23 15:23:59 +0600

    Update orders.js
    connected to database and made POST GET DELETE http verbs functional

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Order = require("../models/order");
+const { default: mongoose } = require("mongoose");
 
 router.get("/", (req, res, next) => {
   res.status(200).json({
@@ -8,30 +10,58 @@ router.get("/", (req, res, next) => {
 });
 
 router.post("/", (req, res, next) => {
-  const order = {
-    productId: req.body.productId,
+  const order = new order({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
     quantity: req.body.quantity,
-  };
-
-  res.status(201).json({
-    //201 means 'everything was successful resourse created'
-    message: "hitting the POST for orders. So, order created, I guess",
-    order: order,
   });
+  order
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.status(201).json({
+        message: "created order via POST",
+        createdOrder: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.get("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "sending back some order details[GET VERB]",
-    orderId: req.params.orderId,
-  });
+  const id = req.params.orderId;
+  Order.findById(id)
+    .exec()
+    .then((doc) => {
+      console.log("from db", doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res
+          .status(404)
+          .json({ message: "No valid entry found for Provided ID" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.delete("/:orderId", (req, res, next) => {
-  res.status(200).json({
-    message: "order deleted [DELETE VERB]",
-    orderId: req.params.orderId,
-  });
+  const id = req.params.orderId;
+  Order.deleteMany({ _id: id })
+    .exec()
+    .then((result) => {
+      console.log("deleted order given via params:id ", result);
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 module.exports = router;
