9942a16f4ea5057d0fa8458829e41d315a103a52
harshahossain <harshahossain@gmail.com>
2023-11-24 21:24:48 +0600

    Validated Users
    added validation for handling email and password

--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,13 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  email: { type: String, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  }, //unique is here for performance cause it doesnt really validates like required does. need to search up email regex though
   password: { type: String, required: true },
 });
 
--- api/routes/users.js
+++ api/routes/users.js
@@ -5,32 +5,60 @@ const bcrypt = require("bcrypt");
 
 const User = require("../models/user");
 
+//POST creating  user
 router.post("/signup", (req, res, nenxt) => {
-  bcrypt.hash(req.body.password, 10, (err, hash) => {
-    if (err) {
-      return res.status(500).json({ error: err });
-    } else {
-      const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: hash,
-      });
-      user
-        .save()
-        .then((result) => {
-          console.log(result);
-          res.status(201).json({
-            message: "User Created",
-          });
-        })
-        .catch((err) => {
-          console.log(err);
-          res.status(500).json({
-            error: err,
-          });
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length >= 1) {
+        return res.status(409).json({
+          message: "Email Exists",
         });
-    }
-  });
+      } else {
+        bcrypt.hash(req.body.password, 10, (err, hash) => {
+          if (err) {
+            return res.status(500).json({ error: err });
+          } else {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+            });
+            user
+              .save()
+              .then((result) => {
+                console.log(result);
+                res.status(201).json({
+                  message: "User Created",
+                });
+              })
+              .catch((err) => {
+                console.log(err);
+                res.status(500).json({
+                  error: err,
+                });
+              });
+          }
+        });
+      }
+    });
+});
+
+//DELETE
+router.delete("/:userId", (req, res, next) => {
+  User.deleteMany({ _id: req.params.userId })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "User Deleted",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
